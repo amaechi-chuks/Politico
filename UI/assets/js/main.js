13347d3590199e34c88639ba3ed12d749097c6a9
chuks_nlog <amaechichuks2000@yahoo.com>
2019-01-19 13:34:18 +0100

    [feature #163301867] create a page for user to vote which contains the following: - All political party for a user to select one - All participating andidates for a user to select one - All political offices for a user to select one - All office names - location input field for a user to enter his location [Delivers #163301867]

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -32,3 +32,71 @@ modalActionBtns.forEach(modalActionBtn => modalActionBtn.addEventListener('click
         toggleModal(evt);
     }
 }));
+
+const updateStatusBtns = Array.from(document.querySelectorAll('.update-record'));
+if (updateStatusBtns) {
+    const toast = document.querySelector('.toast');
+    updateStatusBtns.forEach(updateStatusBtn => updateStatusBtn.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        toast.classList.toggle('toast-show');
+        setTimeout(() => {
+            toast.classList.remove('toast-show');
+        }, 3000);
+    }));
+}
+
+const toastHide = document.querySelector('.toast-hide');
+if (toastHide) {
+    const toast = document.querySelector('.toast');
+    toastHide.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        toast.classList.toggle('toast-show');
+    });
+}
+
+const closeModal = document.querySelector('.modal-close');
+if (closeModal) {
+    closeModal.addEventListener('click', (evt) => {
+        toggleModal(evt);
+    });
+}
+
+const submitBtn = document.querySelector('.update');
+if (submitBtn) {
+    submitBtn.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        let message = '';
+
+        if (evt.target.id === 'record') {
+            message = 'Record Updated Successfully';
+        } else if (evt.target.id === 'profile') {
+            message = 'Profile Updated Successfully';
+        } else if (evt.target.id === 'voted') {
+            message = 'Voted Successfully';
+        }
+        else if (evt.target.id === 'records') {
+            message = 'Updated Successfully';
+        }
+        else if (evt.target.id === 'signup') {
+            message = 'Signup Successful, Please Log In to continue';
+        }
+
+        document.querySelector('.message-box').textContent = message;
+    });
+}
+
+const mediaLinks = document.querySelectorAll('.report-media');
+mediaLinks.forEach((mediaLink) => {
+    mediaLink.addEventListener('click', (evt) => {
+        toggleModal(evt);
+    });
+});
+
+function submitForm() {
+    window.location.href = './user-profile.html';
+    return false;
+}
+function submitFormAdmin() {
+    window.location.href = './admin-profile.html';
+    return false;
+}
