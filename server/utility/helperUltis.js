bc424f7a6f691ecc40a0abb83cbe574c1c6b325b
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-01-26 00:15:28 +0100

    feature[API endpoint #163420503] Set up signup REST API endpoint(CRUD) (#32)
    * feature[API endpoint #163420503] Set up signup REST API endpoint(CRUD)
    [Delivers #163420503]
    
    feature[API endpoint]Implement validation modules
    [Delivers #163420503]
    
    * fix(UI template): Implement LFA feedback on UI template design (#33)
    
    Add apply page for a user to participate as a candidate
    Add a office record page for an admin to view
    Add awesome font icons
    [Finishes | Delivers #163375971]
    
    * #163345966 Build out GET REST Endpoints for all Offices (CRUD) (#29) (#30)
    
    * feat[API endpoints #163345966]Set up GET offices endpoint for all
    offices
    [Finishes|Deliver #163345966]
    
    * feature[API endpoint] Set up GET REST API endpoint(CRUD)
    
    * [chore] Implement LFA feedback on naming convention
    
    * fix(UI template): Implement LFA feedback on UI template design (#33)
    
    Add apply page for a user to participate as a candidate
    Add a office record page for an admin to view
    Add awesome font icons
    [Finishes | Delivers #163375971]
    
    * fix(UI template): Implement LFA feedback on UI template design (#33)
    
    Add apply page for a user to participate as a candidate
    Add a office record page for an admin to view
    Add awesome font icons
    [Finishes | Delivers #163375971]

--- server/utility/helperUltis.js
+++ server/utility/helperUltis.js
@@ -7,7 +7,7 @@ class HelperUtils {
     return {
       name: /^[a-zA-Z]+$/,
       email: /^([A-z0-9]+)([._-]{0,1})([A-z0-9]+)@([A-z0-9-_.]+)\.([A-z]{2,3})$/,
-      phonenumber: /^[+\d\-\s]+$/,
+      phoneNumber: /^[+\d\-\s]+$/,
       location: /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/,
       hqAddress: /^\d+\s[A-z]+\s[A-z]+/g,
       logoUrl: /\.(gif|jpg|jpeg|tiff|png|mp4)$/i,
