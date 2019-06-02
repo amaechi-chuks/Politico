280a0141038ca030a4d444d5bdddd9e007911033
Amaechi Chuks <amaechichuks2000@yahoo.com>
2019-06-02 18:34:16 +0100

    chore(update): Update route folder

--- server/route/routes.js
+++ server/route/routes.js
@@ -15,7 +15,6 @@ const router = express.Router();
 
 // Handle all Post request
 router.post('/parties',
-  // AuthenticateUser.verifyAdmin,
   Validate.validateIfPartyExist,
   Validate.validateIfPartyLogoExist,
   Validate.validateHqAddress,
@@ -27,7 +26,6 @@ router.post('/profile_pic',
   AuthenticateUser.verifyUser, middleware.updateUserPic);
 
 router.post('/offices',
-  AuthenticateUser.verifyAdmin,
   Validate.validateIfOfficeExist,
   Validate.validateOfficeType,
   Validate.validateName,
@@ -49,23 +47,22 @@ router.post('/office/:id/register',
   Validate.validateExistingCandidate,
   CandidateController.createCandidate);
 
-router.post('/votes', AuthenticateUser.verifyUser, Validate.validateCandidacy, VoteController.createVote);
+router.post('/votes', Validate.validateCandidacy, VoteController.createVote);
 
 //  Handle all Get request
 router.get('/parties', PartyController.getAllParty);
 router.get('/parties/:id', AuthenticateUser.verifyUser, Validate.findById, PartyController.getPartyById);
 router.get('/offices', OfficeController.getAllOffice);
-router.get('/offices/:id', AuthenticateUser.verifyUser, Validate.findById, OfficeController.getOfficeById);
-router.get('/office/:id/result', AuthenticateUser.verifyUser, OfficeController.getOfficeResultById);
+router.get('/offices/:id', Validate.findById, OfficeController.getOfficeById);
+router.get('/office/:id/result', OfficeController.getOfficeResultById);
 router.get('/auth/user/:id', AuthenticateUser.verifyUser, UserController.fetchUser);
 router.get('/interest', InterestController.fetchAllInterestedUsers);
-router.get('/interest/:id', AuthenticateUser.verifyUser, InterestController.fetchInterestedUserById);
+router.get('/interest/:id', InterestController.fetchInterestedUserById);
 
 //  Handle all Patch request
 router.patch('/parties/:id', Validate.validateName, Validate.findById, PartyController.updateName);
 
 //  Handles all delete request
-router.delete('/parties/:id', AuthenticateUser.verifyAdmin,
-  AuthenticateUser.verifyUser, Validate.findById, PartyController.deletePartyById);
+router.delete('/parties/:id', Validate.findById, PartyController.deletePartyById);
 
 export default router;
